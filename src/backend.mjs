51330613278a769748793b3236c5aa8a2f6b7f16
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-01-31 10:36:02 +0100

    backend.mjs

---
+++ src/backend.mjs
@@ -0,0 +1,19 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('maison').getFullList({
+            sort: '-created',
+        });
+        data = data.map((collection) => {
+            collection.img = pb.files.getURL(collection, collection.images);
+            return data;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
