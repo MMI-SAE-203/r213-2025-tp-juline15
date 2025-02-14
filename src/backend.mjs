0ce19d6a5258c375d28f3e0b62d2a7392b4dfe58
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-02-14 10:48:12 +0100

    filtrer les offres par prix

--- src/backend.mjs
+++ src/backend.mjs
@@ -58,3 +58,20 @@ export async function addOffre(house) {
         };
     }
 }
+
+export async function filterByPrix(prixMin, prixMax) {
+    try {
+        let data = await pb.collection('maison').getFullList({
+            sort: '-created',
+            filter: `prix >= ${prixMin} && prix <= ${prixMax}`
+        });
+        data = data.map((maison) => {
+            maison.imageUrl = pb.files.getURL(maison, maison.images);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en filtrant la liste des maisons', error);
+        return [];
+    }
+}
